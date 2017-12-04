5d5aba668cd4e45fdfc4dbf94a38f83f2a12d52f
Germtao <TAO@MbPro.local>
2017-12-04 18:17:58 +0800

    Sb cell 布局控件&Stopwatch创建

---
+++ Project 03 - Stopwatch/Stopwatch/Stopwatch/Stopwatch.swift
@@ -0,0 +1,16 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//  Created by TAO on 2017/12/4.
+//  Copyright © 2017年 ShaggyT. All rights reserved.
+//
+
+import UIKit
+
+class Stopwatch: NSObject {
+
+    var counter: Double = 0.0
+    var timer: Timer = Timer()
+    
+}
